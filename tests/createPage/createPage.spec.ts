a50fa9208a8394395ee3e685c9b3142b942ad9e4
Nort <4knort@gmail.com>
2022-09-27 12:45:47 +0300

    added tests to create page

--- tests/createPage/createPage.spec.ts
+++ tests/createPage/createPage.spec.ts
@@ -1,5 +1,9 @@
 import { test, expect } from '@playwright/test';
 
+const wait = (ms: number) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 test.describe('новый пользователь открывает страницу create', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("https://4knort.github.io/colorizr");
@@ -30,123 +34,242 @@ test.describe('новый пользователь открывает стран
     }
   })
 
-  test('видит панель с 10 элементами готовых темных цветов', () => {
+  test('видит панель с 10 элементами готовых темных цветов', async ({ page }) => {
+    const items = await page.locator('.panel__colors >> nth=0 >> .panel__color-item')
 
+    const items_count = await items.count();
+    await expect(items_count).toEqual(10)
   })
 
-  test('видит кнопку select all в панеле с темными цветами', () => {
+  test('видит кнопку select all в панеле с темными цветами', async ({ page }) => {
+    const btn = await page.locator('.panel__btn >> nth=0')
 
+    await expect(btn).toBeVisible()
   })
 
-  test('видит панель с 10 готовыми смешанными цветами', () => {
+  test('видит панель с 10 готовыми смешанными цветами', async ({ page }) => {
+    const items = await page.locator('.panel__colors >> nth=1 >> .panel__color-item')
 
+    const items_count = await items.count();
+    await expect(items_count).toEqual(10)
   })
 
-  test('видит кнопку select all в панеле со смешанными цветами', () => {
+  test('видит кнопку select all в панеле со смешанными цветами', async ({ page }) => {
+    const btn = await page.locator('.panel__btn >> nth=1')
 
+    await expect(btn).toBeVisible()
   })
 
-  test('видит кнопку открытие коло пикера в панеле смешанных цветов', () => {
+  test('видит кнопку открытие колор пикера в панеле смешанных цветов', async ({ page }) => {
+    const colorPickerBtn = await page.locator('.panel__color-block')
 
+    await expect(colorPickerBtn).toBeVisible()
   })
 
-  test('видит что в панеле смешанных цветов на элементах нет крестика', () => {
+  test('видит что в панеле смешанных цветов на элементах нет крестика', async ({ page }) => {
+    const items = await page.locator('.panel__colors >> nth=1 >> .panel__color-item >> .panel__color-icon')
+    const items_count = await items.count();
 
+    for (let i = 0; i < items_count; i++) {
+      const loop_item = items.nth(i);
+
+      await expect(loop_item).toHaveCSS('display', 'none');
+    }
   })
 
-  test('видит что в панеле темных цветов на элементах нет крестика', () => {
+  test('видит что в панеле темных цветов на элементах нет крестика', async ({ page }) => {
+    const items = await page.locator('.panel__colors >> nth=0 >> .panel__color-item >> .panel__color-icon')
 
+    const items_count = await items.count();
+    for (let i = 0; i < items_count; i++) {
+      const loop_item = items.nth(i);
+
+      await expect(loop_item).toHaveCSS('display', 'none');
+    }
   })
 
   test.describe(`в панеле темных цветов нажимает на кнопку select all`, () => {
+    test.beforeEach(async ({ page }) => {
+      const btn = await page.locator('.panel__btn >> nth=0')
 
-    test(`в панеле темных цветов нажимает на кнопку select all
-      в панеле выбора цветов все элементы окрасились соответствуя цветам в панеле темных цветов
-      нажимает на первый элемент в панеле смешанных цветов
-      видит что последний элемент в панеле выбора цвета окрасился в тот же цвет`, () => {
+      await btn.click()
+      await wait(1000) // color changes to slow, playwright is too fast
+    });
 
+    test(`в панеле выбора цветов все элементы окрасились соответствуя цветам в панеле темных цветов`, async ({ page }) => {
+      const itemsChoose = await page.locator('.panel__color-item--choose');
+      const itemsColor = await page.locator('.panel__colors >> nth=0 >> .panel__color-item')
+
+      const items_count = await itemsChoose.count();
+
+      for (let i = 0; i < items_count; i++) {
+        const bgChooseItem = await itemsChoose.nth(i).evaluate((element) => {
+          return window.getComputedStyle(element).getPropertyValue('background-color')
+        })
+
+        const bgColorItem = await itemsColor.nth(i).evaluate((element) => {
+          return window.getComputedStyle(element).getPropertyValue('background-color')
+        })
+
+        await expect(bgChooseItem).toEqual(bgColorItem);
+      }
     })
 
     test.describe('нажимает на первый элемент в панеле смешанных цветов', () => {
-      test(' видит что последний элемент в панеле выбора цвета окрасился в тот же цвет', () => {
+      test.beforeEach(async ({ page }) => {
+        const itemColor = await page.locator('div:nth-child(4) > .panel__colors > div:nth-child(2)');
+        await itemColor.click()
+        await wait(1000) // color changes to slow, playwright is too fast
+      })
+      test(' видит что последний элемент в панеле выбора цвета окрасился в тот же цвет', async ({ page }) => {
+        const itemChoose = await page.locator('.panel__color-item--choose >> nth=9');
+        const itemColor = await page.locator('div:nth-child(4) > .panel__colors > div:nth-child(2)');
 
+        const bgChooseItem = await itemColor.evaluate((element) => {
+          return window.getComputedStyle(element).getPropertyValue('background-color')
+        })
+
+        const bgColorItem = await itemChoose.evaluate((element) => {
+          return window.getComputedStyle(element).getPropertyValue('background-color')
+        })
+
+        expect(bgChooseItem).toEqual(bgColorItem)
       })
     })
   })
 
 
   test.describe(`в панеле смешанных цветов нажимает на кнопку select all`, () => {
+    test.beforeEach(async ({ page }) => {
+      const btn = await page.locator('.panel__btn >> nth=1')
 
-    test(`в панеле выбора цветов все элементы окрасились соответствуя цветам в панеле смешанных цветов`, () => {
+      await btn.click()
+      await wait(1000) // color changes to slow, playwright is too fast
+    });
+    test(`в панеле выбора цветов все элементы окрасились соответствуя цветам в панеле смешанных цветов`, async ({ page }) => {
+      const itemsChoose = await page.locator('.panel__color-item--choose');
+      const itemsColor = await page.locator('.panel__colors >> nth=0 >> .panel__color-item')
 
+      const items_count = await itemsChoose.count();
+
+      for (let i = 0; i < items_count; i++) {
+        const bgChooseItem = await itemsChoose.nth(i).evaluate((element) => {
+          return window.getComputedStyle(element).getPropertyValue('background-color')
+        })
+
+        const bgColorItem = await itemsColor.nth(i).evaluate((element) => {
+          return window.getComputedStyle(element).getPropertyValue('background-color')
+        })
+
+        await expect(bgChooseItem).toEqual(bgColorItem);
+      }
     })
   })
 
   test.describe(`наводит на элемент в панеле темных цветов`, () => {
+    test.beforeEach(async ({ page }) => {
+      await page.locator('div:nth-child(3) > .panel__colors > div').first().hover();
+    });
 
-    test(`видит что появился крестик`, () => {
-
+    test(`видит что появился крестик`, async ({ page }) => {
+      const span = await page.locator('div:nth-child(3) > .panel__colors > div > .panel__color-icon >> nth=0')
+      await expect(span).toHaveCSS('display', 'block')
     })
 
     test.describe('убирает курсор с элемента', () => {
-      test('видит что крестика нет', () => {
-
+      test.beforeEach(async ({ page }) => {
+        await page.locator('div:nth-child(3) > .panel__colors > div').last().hover();
+      });
+      test('видит что крестика нет', async ({ page }) => {
+        const span = await page.locator('div:nth-child(3) > .panel__colors > div > .panel__color-icon >> nth=0')
+        await expect(span).toHaveCSS('display', 'none')
       })
     })
   })
 
   test.describe(`наводит на элемент в панеле смешанных цветов`, () => {
-
-    test(`видит что появился крестик`, () => {
-
+    test.beforeEach(async ({ page }) => {
+      await page.locator('div:nth-child(4) > .panel__colors > div:nth-child(2)').hover();
+    });
+    test(`видит что появился крестик`, async ({ page }) => {
+      const span = await page.locator('div:nth-child(4) > .panel__colors > div:nth-child(2) > .panel__color-icon >> nth=0')
+      await expect(span).toHaveCSS('display', 'block')
     })
 
     test.describe('убирает курсор с элемента', () => {
-      test('видит что крестика нет', () => {
-
+      test.beforeEach(async ({ page }) => {
+        await page.locator('div:nth-child(3) > .panel__colors > div').last().hover();
+      });
+      test('видит что крестика нет', async ({ page }) => {
+        const span = await page.locator('div:nth-child(4) > .panel__colors > div:nth-child(2) > .panel__color-icon >> nth=0')
+        await expect(span).toHaveCSS('display', 'none')
       })
     })
   })
 
   test.describe(`нажимает на первый элемент в панеле темных цветов`, () => {
+    test.beforeEach(async ({ page }) => {
+      await page.locator('div:nth-child(3) > .panel__colors > div').first().click();
+      await wait(1000) // color changes to slow, playwright is too fast
+    });
 
-    test(`видит что первый элемент в панеле выбора цветов окрасился в тот же цвет`, () => {
+    test(`видит что первый элемент в панеле выбора цветов окрасился в тот же цвет`, async ({ page }) => {
+      const itemChoose = await page.locator('.panel__color-item--choose >> nth=0');
+      const itemColor = await page.locator('div:nth-child(3) > .panel__colors > div >> nth=0')
 
+      const bgChooseItem = await itemChoose.evaluate((element) => {
+        return window.getComputedStyle(element).getPropertyValue('background-color')
+      })
+
+      const bgColorItem = await itemColor.evaluate((element) => {
+        return window.getComputedStyle(element).getPropertyValue('background-color')
+      })
+
+      await expect(bgChooseItem).toEqual(bgColorItem)
     })
 
-    test(`видит что на элементе появился крестик`, () => {
-
+    test(`видит что на элементе появился крестик`, async ({ page }) => {
+      const span = await page.locator('div:nth-child(3) > .panel__colors > div > .panel__color-icon >> nth=0')
+      await expect(span).toHaveCSS('display', 'block')
     })
 
     test.describe('наводит на покрашенный элемент в выборе цветов', () => {
-      test('видит что на нем появился крестик', () => {
+      test.beforeEach(async ({ page }) => {
+        await page.locator('.panel__color-item--choose >> nth=0').hover();
+      });
 
+      test('видит что на нем появился крестик', async ({ page }) => {
+        const span = await page.locator('text=Select up to 10 colors Select Colors by clicking on them+ >> span');
+
+        await expect(span).toHaveCSS('display', 'block')
+      })
+      test.describe('уводит курсор на другой элемент', () => {
+        test.beforeEach(async ({ page }) => {
+          await page.locator('.panel__color-item--choose').last().hover();
+        });
+        test('видит что крестика нет', async ({ page }) => {
+          const span = await page.locator('text=Select up to 10 colors Select Colors by clicking on them+ >> span');
+
+          await expect(span).toHaveCSS('display', 'none')
+        })
       })
     })
 
     test.describe('нажимает на покрашенный элемент в выборе цветов', () => {
-      test('видит что он стал фон элемента стал прозрачным rgba(245. 245. 245)', () => {
-
-      })
-    })
-
-    test.describe('уводит курсор на другой элемент', () => {
-      test('видит что на выбранном элементе все равно виден крестик', () => {
-
+      test.beforeEach(async ({ page }) => {
+        await page.locator('.panel__color-item--choose >> nth=0').click();
+      });
+      test('видит что он стал фон элемента стал прозрачным rgb(245. 245. 245)', async ({ page }) => {
+        const colorItem = await page.locator('.panel__color-item--choose >> nth=0');
+        await expect(colorItem).toHaveCSS('background-color', 'rgb(245, 245, 245)')
       })
     })
   })
 
-  test.describe('в панеле смешанных цветов видит кнопку для колор пикера', () => {
-    test.describe('нажимает на нее', () => {
-      test('видит color picker', () => {
-
-      })
-      test.describe('', () => {
-        test('видит что color picker исчез', () => {
-
-        })
-      })
+  test.describe('в панеле смешанных цветов', () => {
+    test('видит кнопку для колор пикера', async ({ page }) => {
+      const colorPickerBtn = await page.locator('text=Mixed with >> span');
+      await expect(colorPickerBtn).toBeVisible()
     })
   })
 });
